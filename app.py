0e3cc7acd0610ad1b08851300df4b63020150988
sejoeder <131190426+sejoeder@users.noreply.github.com>
2023-04-18 19:45:36 +0000

    Added bootstrap and jobitems on HTML page

--- app.py
+++ app.py
@@ -2,9 +2,27 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Geiler Typ Vollzeit',
+    'location': 'bei mir, Wien',
+  },
+  {
+    'id': 2,
+    'title': 'Rambo Ramon Rainer Vollzeit',
+    'location': 'bei mir, Wien',
+  },
+  {
+    'id': 3,
+    'title': 'Hure der Reichen',
+    'location': 'OEVP, Wien',
+  }
+]
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company_name='Sewe')
 
 if __name__ == "__main__":
   app.run(host = '0.0.0.0', debug=True)
