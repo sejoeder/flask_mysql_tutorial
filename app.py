c0e73b1a0ef95d2e9516b59c617c7f2a7e64811a
sejoeder <131190426+sejoeder@users.noreply.github.com>
2023-04-22 21:28:46 +0000

    load jobs from DB

--- app.py
+++ app.py
@@ -1,32 +1,21 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_database
+
 
 app = Flask(__name__)
 
-JOBS = [
-  {
-    'id': 1,
-    'title': 'Test Scientist',
-    'location': 'Test1, Vienna',
-  },
-  {
-    'id': 2,
-    'title': 'Rambo Ramon Rainer Vollzeit',
-    'location': 'bei mir, Wien',
-  },
-  {
-    'id': 3,
-    'title': 'Test Analyst',
-    'location': 'Test2, Vienna',
-  }
-]
 
+
+  
 @app.route("/")
 def hello_world():
-  return render_template('home.html', jobs=JOBS, company_name='Sewe')
+  jobs = load_jobs_from_database()
+  return render_template('home.html', jobs=jobs, company_name='Sewe')
 
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS)
+  jobs = load_jobs_from_database()
+  return jsonify(jobs)
 
 if __name__ == "__main__":
   app.run(host = '0.0.0.0', debug=True)
